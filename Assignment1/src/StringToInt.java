04e62b82a269979bb450af58935de80037aaa8db
ajinkya599 <ajinkya599@gmail.com>
2016-09-05 23:08:01 +0530

    added class to convert binary string to its integer value

---
+++ Assignment1/src/StringToInt.java
@@ -0,0 +1,16 @@
+public class StringToInt {
+
+	public int convert(String str) {
+		int val = Integer.parseInt(str, 2);
+		return val;
+	}
+	public static void main(String[] args) {
+		
+		String bin = "00020111";
+		StringToInt obj = new StringToInt();
+		int res = obj.convert(bin);
+		System.out.println(res);
+
+	}
+
+}
