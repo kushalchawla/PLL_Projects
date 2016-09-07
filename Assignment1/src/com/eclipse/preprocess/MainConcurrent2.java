b1d5d55c9aa91e88942fdd70005e27edbdcd492a
kushalchawla <kushalchawla18@gmail.com>
2016-09-07 14:40:54 +0530

    Main for part 2 using concurrency package completed and working fine.

--- Assignment1/src/com/eclipse/preprocess/MainConcurrent2.java
+++ Assignment1/src/com/eclipse/preprocess/MainConcurrent2.java
@@ -10,6 +10,7 @@ package com.eclipse.preprocess;
 import java.util.Arrays;
 import java.util.LinkedList;
 import java.util.Queue;
+import java.util.concurrent.CountDownLatch;
 import java.util.concurrent.Executor;
 import java.util.concurrent.Executors;
 import java.util.concurrent.Semaphore;
@@ -22,6 +23,7 @@ class GlobalInfo2 {
 	public static Queue<int[]> pipeLine = new LinkedList<int[]>();
 	public static Object enqueueLock = new Object();
 	public static Semaphore available = new Semaphore(1);
+	public static Executor pool = Executors.newCachedThreadPool();
 }
 
 /**
@@ -29,14 +31,17 @@ class GlobalInfo2 {
  */
 class Sensor2 implements Runnable {
 	int deviceNumber;
+	CountDownLatch startSignal;
 	
 	/**
      * Constructor to set the thread number.
      * 
-     * @param threadNumber the order number of the current thread.
+     * @param deviceNumber the order number of the current thread.
+     * @param startSignal Signal to start the processing.
      */
-	public Sensor2(int threadNumber) {
-		deviceNumber = threadNumber;
+	public Sensor2(int deviceNumber, CountDownLatch startSignal) {
+		this.deviceNumber = deviceNumber;
+		this.startSignal = startSignal;
 	}
 	
 	/**
@@ -61,8 +66,11 @@ class Sensor2 implements Runnable {
 				e1.printStackTrace();
 			}
 			System.out.format("%d got the lock.\n",deviceNumber);
-			GlobalInfo.inputs[deviceNumber] = rawData;
-			GlobalInfo.pipeLine.add(GlobalInfo.inputs);				
+			if(GlobalInfo2.inputs[deviceNumber] == -1){
+				startSignal.countDown();
+			}
+			GlobalInfo2.inputs[deviceNumber] = rawData;
+			GlobalInfo2.pipeLine.add(GlobalInfo2.inputs);				
 			
 			GlobalInfo2.available.release();
 			
@@ -77,34 +85,167 @@ class Sensor2 implements Runnable {
 }
 
 /**
- * Class to implement thread for Fusion process
+ * Class to implement thread for addition fusion process
  */
-class DataFusion2 implements Runnable {
+class fuseAdd2 implements Runnable {
+	int[] sortedSnapshot;
+	CountDownLatch workDone;
+	/**
+     * Constructor to set the array.
+     * 
+     * @param sortedSnapshot the sorted integer array.
+     * @param workDone Latch to indicate work is done.
+     */
+	public fuseAdd2(int[] sortedSnapshot, CountDownLatch workDone) {
+		this.sortedSnapshot = sortedSnapshot;
+		this.workDone = workDone;
+	}
 	
 	/**
      * Run function of the thread called within start function.
      * 
      */
 	public void run() {
-		boolean flag = true;
-		
-		//Testing if all the threads are initialized with some random value.
-		while(flag) {
-			flag = false;
-			for (int i = 0; i < 10; i++) {
-				if(GlobalInfo.inputs[i]==-1)
-					flag = true;
-			}
+		System.out.println("fuseAdd");
+		AddConcurrent2 adder = new AddConcurrent2();
+		ValidateFusion validator = new ValidateFusion();
+		int sum = 0;
+		try {
+			sum = adder.addConcurrent2(sortedSnapshot);
+		} catch (InterruptedException e1) {
+			// TODO Auto-generated catch block
+			e1.printStackTrace();
+		}			 
+		validator.validate(sum, 2);
+		workDone.countDown();
+		try {
+			Thread.sleep(1000);
+		} catch (InterruptedException e) {
+			e.printStackTrace();
+		}
+	}
+}
+
+/**
+ * Class to implement thread for multiplier fusion process
+ */
+class fuseMultiply2 implements Runnable {
+	int[] sortedSnapshot;
+	CountDownLatch workDone;
+	/**
+     * Constructor to set the array.
+     * 
+     * @param sortedSnapshot the sorted integer array
+     * @param workDone To indicate work is done
+     */
+	public fuseMultiply2(int[] sortedSnapshot, CountDownLatch workDone) {
+		this.sortedSnapshot = sortedSnapshot;
+		this.workDone = workDone;
+	}
+	
+	/**
+     * Run function of the thread called within start function.
+     * 
+     */
+	public void run() {
+		System.out.println("fuseMul");
+		MultiplyConcurrent2 multiplier = new MultiplyConcurrent2();
+		ValidateFusion validator = new ValidateFusion();
+		int mul = 0;
+		try {
+			mul = multiplier.multiplyConcurrent2(sortedSnapshot);
+		} catch (InterruptedException e1) {
+			// TODO Auto-generated catch block
+			e1.printStackTrace();
+		}			 
+		validator.validate(mul, 1);
+		workDone.countDown();
+		try {
+			Thread.sleep(1000);
+		} catch (InterruptedException e) {
+			e.printStackTrace();
+		}
+	}
+}
+
+
+/**
+ * Class to implement thread for average fusion process
+ */
+class fuseAverage2 implements Runnable {
+	int[] sortedSnapshot;
+	CountDownLatch workDone;
+	
+	/**
+     * Constructor to set the array.
+     * 
+     * @param sortedSnapshot the sorted integer array.
+     * @param workDone To indicate the work is done.
+     */
+	public fuseAverage2(int[] sortedSnapshot, CountDownLatch workDone) {
+		this.sortedSnapshot = sortedSnapshot;
+		this.workDone = workDone;
+	}
+	
+	/**
+     * Run function of the thread called within start function.
+     * 
+     */
+	public void run() {
+		System.out.println("fuseAvg");
+		AverageConcurrent2 averager = new AverageConcurrent2();
+		ValidateFusion validator = new ValidateFusion();
+		float avg = 0;
+		try {
+			avg = averager.averageConcurrent2(sortedSnapshot);
+		} catch (InterruptedException e1) {
+			// TODO Auto-generated catch block
+			e1.printStackTrace();
+		}			 
+		validator.validate(avg, 0);
+		workDone.countDown();
+		try {
+			Thread.sleep(1000);
+		} catch (InterruptedException e) {
+			e.printStackTrace();
+		}
+	}
+}
+
+
+
+
+/**
+ * Class to implement thread for Fusion process
+ */
+class DataFusion2 implements Runnable {
+	CountDownLatch startSignal;
+	
+	/**
+	 * A constructor to set startSignal
+	 * @param startSignal Signal to start the processing
+	 */
+	public DataFusion2(CountDownLatch startSignal){
+		this.startSignal = startSignal;
+	}
+	
+	
+	/**
+     * Run function of the thread called within start function.
+     * 
+     */
+	public void run() {
+		//Waiting for all the threads to get initialized with some random value.
+		try {
+			startSignal.await();
+		} catch (InterruptedException e3) {
+			// TODO Auto-generated catch block
+			e3.printStackTrace();
 		}
 		
 		int[] snapshot = new int[10];
 		int[] sortedSnapshot;
-		int sum = 0,mul = 0;
-		float avg = 0;
-		AddConcurrent2 adder = new AddConcurrent2();
-		MultiplyConcurrent2 multiplier = new MultiplyConcurrent2();
-		AverageConcurrent2 averager = new AverageConcurrent2();
-		ValidateFusion validator = new ValidateFusion();		
+				
 		
 		while(true) {
 			
@@ -131,32 +272,28 @@ class DataFusion2 implements Runnable {
 				}
 				System.out.println();
 				
+				CountDownLatch workDone = new CountDownLatch(3);
+				
 				// Performing fusion of data and validating results
-				try {
-					sum = adder.addConcurrent2(sortedSnapshot);
-				} catch (InterruptedException e2) {
-					// TODO Auto-generated catch block
-					e2.printStackTrace();
-				}			 
-				validator.validate(sum, 2);
+				fuseAdd2 adder = new fuseAdd2(sortedSnapshot,workDone);
+				GlobalInfo2.pool.execute(adder);
+			
+				
+				fuseMultiply2 multiplier = new fuseMultiply2(sortedSnapshot,workDone);
+				GlobalInfo2.pool.execute(multiplier);
+				
+				fuseAverage2 averager = new fuseAverage2(sortedSnapshot,workDone);
+				GlobalInfo2.pool.execute(averager);
+				
+				
+				System.out.println();
 				
 				try {
-					mul = multiplier.multiplyConcurrent2(sortedSnapshot);
+					workDone.await();
 				} catch (InterruptedException e1) {
 					// TODO Auto-generated catch block
 					e1.printStackTrace();
 				}
-				validator.validate(mul, 1);
-				
-				try {
-					avg = averager.averageConcurrent2(sortedSnapshot);
-				} catch (InterruptedException e1) {
-					// TODO Auto-generated catch block
-					e1.printStackTrace();
-				} 
-				validator.validate(avg, 0);
-				
-				System.out.println();
 				
 				try {
 					Thread.sleep(1000);
@@ -173,7 +310,7 @@ class DataFusion2 implements Runnable {
  * Main Class containing the main function.
  */
 public class MainConcurrent2 {
-	static Executor pool = Executors.newCachedThreadPool();
+	
 	/**
      * Main function of used to create threads and start the process.
      * 
@@ -181,16 +318,19 @@ public class MainConcurrent2 {
      */
 	public static void main(String[] args) {
 		System.out.println("main started...");
+		
+		CountDownLatch startSignal = new CountDownLatch(10);
+		
 		for (int i = 0; i < 10; i++) {
 			GlobalInfo2.inputs[i] = -1;
 		}
 		
-		DataFusion2 processor = new DataFusion2();
-		pool.execute(processor);	
+		DataFusion2 processor = new DataFusion2(startSignal);
+		GlobalInfo2.pool.execute(processor);	
 		
 		for (int threadNo = 0; threadNo < 10; threadNo++) {
-			Sensor2 obj = new Sensor2(threadNo);
-			pool.execute(obj);
+			Sensor2 obj = new Sensor2(threadNo, startSignal);
+			GlobalInfo2.pool.execute(obj);
 		}
 
 	}
